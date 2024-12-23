2a6a5b20fb4d0cf35f4be3b79781608e83110307
eliezer <eliezer884@gmail.com>
2024-12-23 17:38:44 +0200

    add tracing log for reactive

--- src/main/java/com/reuven/jfrog/JfrogApplication.java
+++ src/main/java/com/reuven/jfrog/JfrogApplication.java
@@ -3,6 +3,8 @@ package com.reuven.jfrog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import reactor.core.publisher.Hooks;
+import reactor.tools.agent.ReactorDebugAgent;
 
 @ComponentScan(basePackages = {
         "com.reuven.jfrog.config",
@@ -13,6 +15,8 @@ import org.springframework.context.annotation.ComponentScan;
 public class JfrogApplication {
 
     public static void main(String[] args) {
+        ReactorDebugAgent.init();
+        Hooks.enableAutomaticContextPropagation(); //for tracing log in reactive
         SpringApplication.run(JfrogApplication.class, args);
     }
 
